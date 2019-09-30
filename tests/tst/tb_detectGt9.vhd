13a5dae69877c194952d7f96225acb5a36966e19
Rafael Corsi <rafael.corsi@insper.edu.br>
2019-09-30 12:33:33 -0300

    Update tb_detectGt9.vhd

--- tests/tst/tb_detectGt9.vhd
+++ tests/tst/tb_detectGt9.vhd
@@ -9,43 +9,60 @@ use ieee.std_logic_1164.all;
 library vunit_lib;
 context vunit_lib.vunit_context;
 
-entity tb_mux4 is
+entity tb_detect9Gt is
   generic (runner_cfg : string);
 end entity;
 
-architecture tb of tb_mux4 is
+architecture tb of tb_detect9Gt is
 
-component mux4 is
-  port(
-    x,y     : in STD_LOGIC_VECTOR(3 downto 0);
-    sel : in Std_logic;
-    z       : out STD_LOGIC_VECTOR(3 downto 0)
-    );
-end component;
+  component detectGt9 is
+    port(
+      x       : in STD_LOGIC_VECTOR(3 downto 0);
+      carryin : in Std_logic;
+      adjust  : out STD_LOGIC
+      );
+  end component;
 
 
-signal  x, y, z : STD_LOGIC_VECTOR(3 downto 0);
-signal sel : Std_logic;
+signal  x : STD_LOGIC_VECTOR(3 downto 0);
+signal  carry,adjust : STD_LOGIC;
 
 begin
 
-  u1 : mux4 port map(x, y, sel, z );
+  u1 : detectGt9 port map(x, carry, adjust);
 
   main : process
   begin
     test_runner_setup(runner, runner_cfg);
 
     -- Teste: 1
-    x <= "1010"; y<= "0101"; sel <= '0';
+    x <= "0000"; carry <= '0';
     wait for 200 ps;
-    assert(z = "1010")  report "Falha em teste: 1" severity error;
+    assert(adjust = '0')  report "Falha em teste: 1" severity error;
 
-    x <= "1010"; y<= "0101"; sel <= '1';
+    -- Teste: 1
+    x <= "1001"; carry <= '0';
     wait for 200 ps;
-    assert(z = "0101")  report "Falha em teste: 1" severity error;
+    assert(adjust = '0')  report "Falha em teste: 1" severity error;
+
+    -- Teste: 1
+    x <= "0000"; carry <= '1';
+    wait for 200 ps;
+    assert(adjust = '1')  report "Falha em teste: 1" severity error;
+
+
+    -- Teste: 1
+    x <= "1000"; carry <= '1';
+    wait for 200 ps;
+    assert(adjust = '1')  report "Falha em teste: 1" severity error;
+
+    -- Teste: 1
+    x <= "1010"; carry <= '0';
+    wait for 200 ps;
+    assert(adjust = '1')  report "Falha em teste: 1" severity error;
+
 
     test_runner_cleanup(runner); -- Simulacao acaba aqui
-        test_runner_cleanup(runner); -- Simulacao acaba aqui
 
   end process;
 end architecture;
